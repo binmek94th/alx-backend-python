0b7340112a70e04839ceb3f0a1b889c1cb91672e
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-05-17 12:46:08 +0300

    stream users while filtering

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,31 @@
+from seed import connect_to_prodev
+
+def stream_users_in_batches(batch_size):
+    conn_prodev = connect_to_prodev()
+    cursor = conn_prodev.cursor(dictionary=True)  
+
+    offset = 0
+    while True:
+        query = "SELECT * FROM user_data LIMIT %s OFFSET %s"
+
+        cursor.execute(query, (batch_size, offset))
+        rows = cursor.fetchall()
+
+        if not rows:
+            break
+
+        for row in rows:
+            yield row
+
+        offset +=  batch_size
+
+    cursor.close()
+    conn_prodev.close()
+
+def batch_processing(batch_size):
+    for row in stream_users_in_batches(batch_size):
+        if row['age'] > 25:
+            print(row)
+            
+if __name__ == "__main__":
+    data = batch_processing(50)
