0879a359007b6acbf86511c6d680c511a8d02a2f
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-05-22 15:40:38 +0300

    log_queries

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,1 +1,22 @@
+import sqlite3
+import functools
 
+def log_queries(func):
+    def wrapper(*args, **kwargs):
+        print(f"Calling {func.__name__} with {args} and {kwargs}")
+        result =func(*args, **kwargs)
+        return result
+    
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+users = fetch_all_users(query="SELECT * FROM users")
+print(users)
