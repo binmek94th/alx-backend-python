ceb718198eeafce774d619482efaebd85a7edf67
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-05-23 13:02:10 +0300

    context managers for database connection and query execution

--- python-context-async-perations-0x02/1-execute.py
+++ python-context-async-perations-0x02/1-execute.py
@@ -1,24 +1,25 @@
 import sqlite3
 
 class DatabaseConnection:
-    def __init__(self, query):
+    def __init__(self, query, **kwargs):
         self.conn = None
         self.query = query
         self.cursor = None
+        self.kwargs = kwargs
 
     def __enter__(self):
         self.conn = sqlite3.connect('users.db')
         self.cursor = self.conn.cursor()
+        self.cursor.execute(self.query, tuple(self.kwargs.values()))
         return self.cursor
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.conn.close()
 
 
-query = "SELECT * FROM users"
+query = "SELECT * FROM users WHERE age > ?"
 
-with DatabaseConnection(query) as cursor:
-    cursor.execute(query)
+with DatabaseConnection(query, age=25) as cursor:
     results = cursor.fetchall()
     for row in results:
         print(row)
