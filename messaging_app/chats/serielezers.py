df61fc1f1053abc51bfe6b65772060edbcfd102d
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-05-28 13:15:21 +0300

    added serializer

---
+++ messaging_app/chats/serielezers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+from chats.models import User, Conversation, Message
+
+
+class UsersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'age']
+        read_only_fields = ['user_id']
+
+
+class ConversationSerializer(serializers.Serializer):
+    participants = UsersSerializer(many=True, read_only=True)
+
+    class Meta:
+        model: Conversation
+        fields = '__all__'
+        read_only_fields = ['conversation_id']
+
+
+class MessageSerializer(serializers.Serializer):
+
+    class Meta:
+        model = Message
+        fields = '__all__'
+        read_only_fields = ['message_id', 'sent_at', 'created_at']
