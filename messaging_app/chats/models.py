47ba74267681eb6abd077182e67bdad029f2e1ff
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-05-28 13:23:05 +0300

    added serializer

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -16,6 +16,7 @@ class Conversation(models.Model):
     conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     participants = models.ManyToManyField(User, related_name='conversations')
 
+
 class Message(models.Model):
     message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -4,18 +4,20 @@ from .models import User, Conversation, Message
 
 
 class UsersSerializer(serializers.ModelSerializer):
+    custom_name = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'age']
         read_only_fields = ['user_id']
 
 
-class ConversationSerializer(serializers.Serializer):
+class ConversationSerializer(serializers.ModelSerializer):
     participants = UsersSerializer(many=True, read_only=True)
     messages = serializers.SerializerMethodField()
 
     class Meta:
-        model: Conversation
+        model= Conversation
         fields = '__all__'
         read_only_fields = ['conversation_id']
 
@@ -24,9 +26,13 @@ class ConversationSerializer(serializers.Serializer):
         return MessageSerializer(messages, many=True).data
 
 
-class MessageSerializer(serializers.Serializer):
-
+class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = '__all__'
         read_only_fields = ['message_id', 'sent_at', 'created_at']
+
+        def validate(self, data):
+            if data.get('text') and 'spam' in data['text'].lower():
+                raise serializers.ValidationError("Messages cannot contain spam.")
+            return data
