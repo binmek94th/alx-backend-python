9bcaaaca9804846751d73948af02464ae37b894d
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-05-28 13:06:26 +0300

    created models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    age = models.DateField()
+
+
+class Conversation(models.Model):
+    user1 = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    user2 = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['timestamp']
+
