afafd735a58e272649cbd625f2cd26d153b68dbd
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-05-30 22:17:12 +0300

    check for error raise

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -12,3 +12,14 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, result, nested_map, path):
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, result)
+
+
+    @parameterized.expand([
+        ({}, ('a')),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+            
+        self.assertEqual(str(cm.exception), f"'{path[-1]}'")
