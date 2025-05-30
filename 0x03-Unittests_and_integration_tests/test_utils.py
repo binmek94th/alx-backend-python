fd38dc31308f71165069083f14b6e1a009d35b8a
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-05-30 22:54:50 +0300

    test memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest.mock import patch, Mock
 
-# class TestAccessNestedMap(unittest.TestCase):
-#     @parameterized.expand([
-#         (1, {"a": 1}, ("a",)),
-#         ({"b": 2}, {"a": {"b": 2}}, ("a",)),
-#         (2, {"a": {"b": 2}}, ("a", "b")),
-#     ])
-#     def test_access_nested_map(self, result, nested_map, path):
-#         result = access_nested_map(nested_map, path)
-#         self.assertEqual(result, result)
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        (1, {"a": 1}, ("a",)),
+        ({"b": 2}, {"a": {"b": 2}}, ("a",)),
+        (2, {"a": {"b": 2}}, ("a", "b")),
+    ])
+    def test_access_nested_map(self, result, nested_map, path):
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, result)
 
 
-#     @parameterized.expand([
-#         ({}, ('a')),
-#         ({"a": 1}, ("a", "b"))
-#     ])
-#     def test_access_nested_map_exception(self, nested_map, path):
-#         with self.assertRaises(KeyError) as cm:
-#             access_nested_map(nested_map, path)
+    @parameterized.expand([
+        ({}, ('a')),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
             
-#         self.assertEqual(str(cm.exception), f"'{path[-1]}'")
+        self.assertEqual(str(cm.exception), f"'{path[-1]}'")
 
 
 class TestGetJson(unittest.TestCase):
@@ -43,3 +43,20 @@ class TestGetJson(unittest.TestCase):
 
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
+
+class TestMemoize(unittest.TestCase):
+    def test_memozie(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+            
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_mtd:
+            obj = TestClass()
+            self.assertEqual(obj.a_property, 42)
+            self.assertEqual(obj.a_property, 42)
+            mock_mtd.assert_called_once()
+    
