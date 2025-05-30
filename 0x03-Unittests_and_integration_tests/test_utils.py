da726c23baccce8fd2baacabf4b81df2c16ef9da
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-05-30 22:32:19 +0300

    test json

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,26 +5,26 @@ from utils import access_nested_map, get_json
 from unittest.mock import patch
 
 
-class TestAccessNestedMap(unittest.TestCase):
-    @parameterized.expand([
-        (1, {"a": 1}, ("a",)),
-        ({"b": 2}, {"a": {"b": 2}}, ("a",)),
-        (2, {"a": {"b": 2}}, ("a", "b")),
-    ])
-    def test_access_nested_map(self, result, nested_map, path):
-        result = access_nested_map(nested_map, path)
-        self.assertEqual(result, result)
+# class TestAccessNestedMap(unittest.TestCase):
+#     @parameterized.expand([
+#         (1, {"a": 1}, ("a",)),
+#         ({"b": 2}, {"a": {"b": 2}}, ("a",)),
+#         (2, {"a": {"b": 2}}, ("a", "b")),
+#     ])
+#     def test_access_nested_map(self, result, nested_map, path):
+#         result = access_nested_map(nested_map, path)
+#         self.assertEqual(result, result)
 
 
-    @parameterized.expand([
-        ({}, ('a')),
-        ({"a": 1}, ("a", "b"))
-    ])
-    def test_access_nested_map_exception(self, nested_map, path):
-        with self.assertRaises(KeyError) as cm:
-            access_nested_map(nested_map, path)
+#     @parameterized.expand([
+#         ({}, ('a')),
+#         ({"a": 1}, ("a", "b"))
+#     ])
+#     def test_access_nested_map_exception(self, nested_map, path):
+#         with self.assertRaises(KeyError) as cm:
+#             access_nested_map(nested_map, path)
             
-        self.assertEqual(str(cm.exception), f"'{path[-1]}'")
+#         self.assertEqual(str(cm.exception), f"'{path[-1]}'")
 
 
 class TestGetJson(unittest.TestCase):
