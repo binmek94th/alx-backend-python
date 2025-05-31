73bc31414449e40d9817e71b6d21c78cbead3056
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-05-31 12:38:45 +0300

    test client integration testing

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -12,7 +12,6 @@ from fixtures import TEST_PAYLOAD
 
 class TestGithubOrgClient(unittest.TestCase):
     """Test cases for GithubOrgClient"""
-
     @parameterized.expand(['google', 'abc'])
     @patch('client.get_json')
     def test_org(self, org_name, mock_get_json):
@@ -51,7 +50,8 @@ class TestGithubOrgClient(unittest.TestCase):
                 "repos_url": "https://api.github.com/orgs/test/repos"
             }
 
-            with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as mock_org:
+            with patch('client.GithubOrgClient.org', 
+                       new_callable=PropertyMock) as mock_org:
                 mock_org.return_value = {
                     "repos_url": "https://api.github.com/orgs/test/repos"
                 }
@@ -59,7 +59,8 @@ class TestGithubOrgClient(unittest.TestCase):
                 client = GithubOrgClient("test")
                 result = client._public_repos_url
 
-                self.assertEqual(result, "https://api.github.com/orgs/test/repos")
+                self.assertEqual(result, 
+                                 "https://api.github.com/orgs/test/repos")
                 mock_org.assert_called_once()
 
     @parameterized.expand([
