98b46a16a11e995fd020cdcb99ab1db3aa3ea6bb
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-05-31 12:33:15 +0300

    test client integration testing

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -108,3 +108,23 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
     def test_public_repos_with_license(self):
         client = GithubOrgClient("test")
         self.assertEqual(client.public_repos("apache-2.0"), self.apache2_repos)
+
+    def test_public_repos(self):
+        """
+        Test public_repos returns expected repo names.
+        """
+        client = GithubOrgClient("google")
+        self.assertEqual(
+            client.public_repos(),
+            self.expected_repos
+        )
+
+    def test_public_repos_with_license(self):
+        """
+        Test public_repos returns filtered repos by license.
+        """
+        client = GithubOrgClient("google")
+        self.assertEqual(
+            client.public_repos(license="apache-2.0"),
+            self.apache2_repos
+        )
