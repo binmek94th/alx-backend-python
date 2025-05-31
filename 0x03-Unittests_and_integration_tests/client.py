c44cf4471d47ac530649eb1301842c3c96d30210
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-05-31 12:47:42 +0300

    test client integration testing

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -35,10 +35,7 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @patch('client.get_json')
     def test_org(self, org_name, mock_get_json):
-        """
-        Test that GithubOrgClient.org returns correct org data and
-        calls get_json with the right URL.
-        """
+        """Test that GithubOrgClient.org returns the correct payload for each organization."""
         expected = {"login": org_name}
         mock_get_json.return_value = expected
 
