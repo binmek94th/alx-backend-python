c038699450924c2040535c4a5a266532c47cbfd6
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-05-31 12:17:04 +0300

    test client has license

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -70,14 +70,13 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_url.assert_called_once()
             mock_get_json.assert_called_once_with("http://test-url")
 
-
     @parameterized.expand([
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False),
-        ({}, "my_license", False),  
+        ({}, "my_license", False),
     ])
     def test_has_license(self, repo, license_key, expected):
-        """Test static method has_license."""
+        """Test has_license returns correct boolean"""
         result = GithubOrgClient.has_license(repo, license_key)
         self.assertEqual(result, expected)
 
