40bde2725a18d30e2253b6d0d44f0585a7105bbe
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-06-07 17:37:27 +0300

    test middleware

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'chats.middleware.RequestLoggingMiddleware',
     'chats.middleware.RestrictAccessByTimeMiddleware',
     'chats.middleware.RolepermissionMiddleware',
+    'chats.middleware.OffensiveLanguageMiddleware',
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
