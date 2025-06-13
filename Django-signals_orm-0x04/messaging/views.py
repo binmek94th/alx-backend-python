eaae3babc48e4323c9aa36873e57a40ccf91a648
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-06-13 13:29:10 +0300

    Threading

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,5 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+
+from messaging.models import Message
+from messaging.serializers import MessageSerializer
 
 
 class delete_user(APIView):
@@ -11,3 +15,14 @@ class delete_user(APIView):
             return Response({"error": "User not authenticated."}, status=401)
         user.delete()
         return Response({"message": "User account deleted successfully."}, status=204)
+
+
+class MessageViewSet(ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_authenticated:
+            return (Message.objects.filter(sender=user) | Message.objects.filter(receiver=user)).distinct()
+        return Message.objects.none()
