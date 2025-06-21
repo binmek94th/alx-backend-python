ae87b8973cbd2abf4afd957933e8f316b43b1954
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-06-21 13:48:46 +0300

    added dockerfile

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
